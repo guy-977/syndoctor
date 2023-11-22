6fe116454a64d0d8c2d7b1d09aca6dc603e60fb4
Ahmed Al-rufai <aalrefaei93@gmail.com>
2023-11-22 13:36:51 +0200

    Basic structure for the API
    this code is just a basic intuition about how the API will be and not a final functional product

--- seg-api/main.py
+++ seg-api/main.py
@@ -1,6 +1,18 @@
+from flask import Flask
 from totalsegmentator.python_api import totalsegmentator
 
-input_path = 'hip_right.nii.gz'
-output_path = './output'
+import os
 
-totalsegmentator(input_path, output_path)
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+
+def get_segmentation():
+    #To Do: make the input dinamically changing based on the user's input
+    input_path = 'hip_right.nii.gz'
+    output_path = './output'
+    totalsegmentator(input_path, output_path)
+    # To do: return the output result back to the user
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
